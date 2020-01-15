8eab7ceaa751adf2340106fe0f6e0075e7a7208a
Tim Womble <wom17002@byui.edu>
2020-01-14 21:14:44 -0700

    Added "Change Color" button and functionality

--- web/Team Activities/Week 02/Week 02.js
+++ web/Team Activities/Week 02/Week 02.js
@@ -1,3 +1,9 @@
+function changeColor() {
+    var input = document.getElementById("input").value;
+    var div1 = document.getElementById("div1");
+    div1.style.backgroundColor = input;
+}
+
 function buttonAlert() {
     alert("Clicked!");
 }
