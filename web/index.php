86f2296a2f8d2840f89902e07bf2fad6f38a05a0
Tim Womble <wom17002@byui.edu>
2020-01-16 17:15:46 -0700

    Setting up for new homepage

--- web/index.php
+++ web/index.php
@@ -1,5 +1,13 @@
-<?php
+<!DOCTYPE html>
+<html>
 
-  phpinfo();
+<head>
+  <meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>hello</title>
+</head>
 
-?>
+<body>
+<h1>Homepage</h1>
+</body>
+
+</html>
