2e41a884a850babb0145615900e88be6c1ee240c
Tim Womble <wom17002@byui.edu>
2020-01-17 13:30:57 -0700

    Added "Personal Block"

--- web/index.php
+++ web/index.php
@@ -13,13 +13,22 @@
     <a href="assignments.html"><b>Assignments Directory</b></a>
     <span> || </span>
     <a href="index_info.php">PHP Info</a>
-    <hr/>
   </header>
+
+  <hr/>
 
   <div id="server_info">
     <button onclick="getPHPdata()">Server Info</button>
-    <hr/>
   </div>
+
+  <hr/>
+
+  <div id="personal_info">
+    <img src="me.jpg" alt="Tim Womble, circa 2015"/>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+  </div>
+
+  <hr/>
 
   <script>
     // SERVER_NAME, REQUEST_METHOD, REQUEST_TIME, REMOTE_PORT
