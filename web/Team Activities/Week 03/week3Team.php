b497292c49bcc4df59185a153dcc90d2e2eb7859
Tim Womble <wom17002@byui.edu>
2020-01-21 20:45:02 -0700

    Added Week 03 Team Activity

---
+++ web/Team Activities/Week 03/week3Team.php
@@ -0,0 +1,36 @@
+
+<!DOCTYPE html>
+
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+    <title>Week 3 Team PHP</title>
+</head>
+
+<body>
+
+  <div>
+	  Username: <?php echo $_POST["name"];?>
+	</div>
+	<div>
+    Email: <?php echo $_POST["email"];?>
+	</div>
+	<div>
+	  Major: <?php echo $_POST["major"];?>
+	</div>
+	<div>
+	  Comments: <?php echo $_POST["comments"];?>
+	</div>
+	<div> 
+	  Continents:
+	  <?php
+      for ($i = 1; $i < 8; $i++) {
+        if (isset($_POST['continent'.$i])){
+          echo $_POST['continent'.$i];
+        }
+      };
+    ?>
+  </div>
+
+</body>
+</html>
