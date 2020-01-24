7341ae8b698753143f014b8cdcbce7ef75091b50
Tim Womble <wom17002@byui.edu>
2020-01-24 07:38:58 -0700

    Setting up for assignment 03

---
+++ web/Week 03/browseItems.php
@@ -0,0 +1,28 @@
+<?php
+  session_start();
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="stylesheets/style_master.css">
+  <title>Aether Shop</title>
+</head>
+
+<body>
+<h1>Hello World!</h1>
+<!-- This was added through VSCode -->
+</body>
+
+<!-- 
+  Ideas:
+  Each item listed will be an item in a <ul> with a button at the end that says "Add to Cart"
+  Once pressed, that button will make an AJAX call through the JS function addItem()
+  The function will pass a number....
+  Am I going to have to call a different PHP to add each item?
+  Or can I make each button "submit a form" to one PHP file without shifting pages?
+-->
+
+</html>
---
+++ web/Week 03/viewCart.php
@@ -0,0 +1,19 @@
+<?php
+  session_start();
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="stylesheets/style_master.css">
+  <title>Aether Shop</title>
+</head>
+
+<body>
+<h1>Hello World!</h1>
+<!-- This was added through VSCode -->
+</body>
+
+</html>
