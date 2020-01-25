285807c1d8d95c9fb98876f0a34ccc2ea0c820fa
Tim Womble <wom17002@byui.edu>
2020-01-25 15:01:42 -0700

    Debugging rm_item1.php

--- web/Week 03/items/rm_item1.php
+++ web/Week 03/items/rm_item1.php
@@ -1,11 +1,11 @@
 <?php 
   session_start();
   $index = 0;
-  foreach($_SESSION["shoppingCart"] as $item) {
-    if ($item["number"] == 1) {
-      break;
-    }
-    $index++;
-  }
-  unset($_SESSION["shoppingCart"][$index])
+  // foreach($_SESSION["shoppingCart"] as $item) {
+  //   if ($item["number"] == 1) {
+  //     break;
+  //   }
+  //   $index++;
+  // }
+  // unset($_SESSION["shoppingCart"][$index])
 ?>
