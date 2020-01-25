53a00cf8d5ee10bc943db46fbce7b9d603b8840f
Tim Womble <wom17002@byui.edu>
2020-01-25 15:07:32 -0700

    Debuggin part 3

--- web/Week 03/items/rm_item1.php
+++ web/Week 03/items/rm_item1.php
@@ -2,7 +2,7 @@
   session_start();
   $index = 0;
   foreach($_SESSION["shoppingCart"] as $item) {
-    if ($item["number"] == 1) {
+    if ($item->number == 1) {
       break;
     }
     $index++;
