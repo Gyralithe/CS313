c25d3b076914f91999392f9d8ef9e21da3132448
Tim Womble <wom17002@byui.edu>
2020-01-25 15:56:14 -0700

    Trying to stop PHP from changing the format of the final string

--- web/Week 03/items/rm_item1.php
+++ web/Week 03/items/rm_item1.php
@@ -1,11 +1,11 @@
 <?php 
   session_start();
   $index = 0;
-  foreach($_SESSION["shoppingCart"] as $key=>$value) {
-    if ($value->number == 1) {
-      $index = $key;
+  foreach($_SESSION["shoppingCart"] as $item) {
+    if ($item->number == 1) {
       break;
     }
+    $index++;
   }
   unset($_SESSION["shoppingCart"][$index]);
 ?>
