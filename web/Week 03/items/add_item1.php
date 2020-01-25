1403da2340d246415433fdaf98271d75c70b7a96
Tim Womble <wom17002@byui.edu>
2020-01-25 14:49:15 -0700

    Attempting to add items to cart as actual objects, rather than JSON strings

--- web/Week 03/items/add_item1.php
+++ web/Week 03/items/add_item1.php
@@ -1,4 +1,17 @@
-<?php 
-  session_start();
-  array_push($_SESSION["shoppingCart"], '{"name":"One (1) egg", "price":"1.99", "number":1}')
+<?php
+
+session_start();
+class item {
+public $name;
+public $price;
+public $number;
+}
+
+$item = new item();
+$item->name = "One (1) egg";
+$item->price = "1.99";
+$item->number = 1;
+
+array_push($_SESSION["shoppingCart"], $item)
+
 ?>
