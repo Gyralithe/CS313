7e5e934b51ec6d4d64852bab0d4970868f2ccf25
Tim Womble <wom17002@byui.edu>
2020-02-08 14:47:49 -0700

    More setup for all three pages

---
+++ web/Week 05/assignment05_3.php
@@ -0,0 +1,38 @@
+<?php
+  try {
+    $dbUrl = getenv('DATABASE_URL');
+    $dbOpts = parse_url($dbUrl);
+    $dbHost = $dbOpts["host"];
+    $dbPort = $dbOpts["port"];
+    $dbUser = $dbOpts["user"];
+    $dbPassword = $dbOpts["pass"];
+    $dbName = ltrim($dbOpts["path"],'/');
+
+    $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  }
+  catch (PDOException $ex)
+  {
+    echo 'Error!: ' . $ex->getMessage();
+    die();
+  }
+
+  session_start();
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="stylesheets/style_master.css">
+  <title>D&D Database - Classes</title>
+</head>
+
+<body>
+
+  <?php include "./header.html" ?>
+
+</body>
+</html>
